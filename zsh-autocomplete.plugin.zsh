fe9dfbe781db301f77b6c776518b21c75993689f
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-09-14 13:28:30 +0300

    Show file types when completing expansions
    Split compinit code into separate functions.

--- zsh-autocomplete.plugin.zsh
+++ zsh-autocomplete.plugin.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 () {
-  emulate -L zsh -o noshortloops -o warncreateglobal -o extendedglob
+  emulate -L zsh -o extendedglob -o NO_shortloops -o warncreateglobal
 
   # Get access to `functions` & `commands` arrays.
   zmodload zsh/parameter
@@ -9,6 +9,8 @@
   [[ -v functions[.zinit-shade-off] ]] && .zinit-shade-off "${___mode:-load}"
 
   typeset -gU FPATH fpath=( ${${(%):-%x}:A:h}/* $fpath )
+  typeset -gHa _autocomplete__options=(
+    localoptions extendedglob NO_completeinword NO_shortloops warncreateglobal )
 
   .autocomplete.no-op() {
     :
@@ -18,32 +20,32 @@
   functions[compinit]=$functions[.autocomplete.no-op]
   autoload -Uz .autocomplete.compinit
   compdef() {
-    emulate -L zsh -o noshortloops -o warncreateglobal -o extendedglob
+    emulate -L zsh -o extendedglob -o NO_shortloops -o warncreateglobal
     .autocomplete.compinit
     compdef $@
   }
-
-  autoload -Uz .autocomplete.patch
-  .autocomplete.patch add-zsh-hook
 
   # In case we're sourced _after_ `zsh-autosuggestions`
   add-zsh-hook -d precmd _zsh_autosuggest_start
 
   # In case we're sourced _before_ `zsh-autosuggestions`
+  functions[__autocomplete__.add-zsh-hook]=$functions[add-zsh-hook]
   add-zsh-hook() {
     # Prevent `_zsh_autosuggest_start` from being added.
-    [[ ${@[(ie)_zsh_autosuggest_start]} -gt ${#@} ]] && .autocomplete.add-zsh-hook "$@"
+    [[ ${@[(ie)_zsh_autosuggest_start]} -gt ${#@} ]] && __autocomplete__.add-zsh-hook "$@"
   }
 
   autoload -Uz .autocomplete.__init__ && .autocomplete.__init__
-  local module mod
-  for module in config widget key key-binding recent-dirs async; do
-    mod=.autocomplete.$module
-    if ! zstyle -t ':autocomplete:' $module false no off 0; then
+  local mod; for mod in \
+      config widget key key-binding recent-dirs async; do
+    if ! zstyle -t :autocomplete: $mod false no off 0; then
+      mod=.autocomplete.$mod
       autoload -Uz $mod && $mod
     fi
   done
 
   # Workaround for https://github.com/zdharma/zinit/issues/366
   [[ -v functions[.zinit-shade-on] ]] && .zinit-shade-on "${___mode:-load}"
+
+  return 0
 }
